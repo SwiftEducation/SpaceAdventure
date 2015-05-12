d39cf808ae4578a262cd108c133498b9ab186188
Yong Bakos <ybakos@humanoriented.com>
2015-05-12 12:05:30 -0500

    Lesson 5. Extract functionality from main into SpaceAdventure method.

--- SpaceAdventure/main.swift
+++ SpaceAdventure/main.swift
@@ -10,30 +10,3 @@ import Foundation
 let adventure = SpaceAdventure()
 adventure.start()
 
-let numberOfPlanets = 8
-let diameterOfEarth = 24859.82 // In miles, from pole to pole.
-
-println("Welcome to our solar system!")
-println("There are \(numberOfPlanets) planets to explore.")
-println("Your are currently on Earth, which has a circumference of \(diameterOfEarth) miles.")
-
-println("What is your name?")
-let name = getln()
-println("Nice to meet you, \(name). My name is Eliza, I'm an old friend of Siri.")
-
-println("Let's go on an adventure!")
-println("Shall I randomly choose a planet for you to visit? (Y or N)")
-
-var decision = "" // Start with empty String
-while !(decision == "Y" || decision == "N") {
-    decision = getln()
-    if decision == "Y" {
-        println("Ok! Traveling to...")
-        // TOOD: travel to random planet
-    } else if decision == "N" {
-        println("Ok, name the planet you would like to visit...")
-        // TODO: let the user select a planet to visit
-    } else {
-        println("Sorry, I didn't get that.")
-    }
-}
