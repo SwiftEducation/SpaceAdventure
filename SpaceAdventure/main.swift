46a43d8ded263a99305ad95ac392f0fd4c263fe4
Yong Bakos <ybakos@humanoriented.com>
2015-05-12 16:19:23 -0500

    Lesson 15. Refactor PlanetarySystem generation with a dictionary and map.

--- SpaceAdventure/main.swift
+++ SpaceAdventure/main.swift
@@ -7,27 +7,19 @@ This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAl
 
 import Foundation
 
-// TODO: Reduce repetitive code.
-let mercury = Planet(name: "Mercury", description: "A very hot planet, closest to the sun.")
-let venus = Planet(name: "Venus", description: "It's very cloudy here!")
-let earth = Planet(name: "Earth", description: "There is something very familiar about this planet.")
-let mars = Planet(name: "Mars", description: "Known as the red planet.")
-let jupiter = Planet(name: "Jupiter", description: "A gas giant, with a noticeable red spot.")
-let saturn = Planet(name: "Saturn", description: "This planet has beautiful rings around it.")
-let uranus = Planet(name: "Uranus", description: "Strangely, this planet rotates around on its side.")
-let neptune = Planet(name: "Neptune", description: "A very cold planet, furthest from the sun.")
-
 let systemName = "Solar System"
-var planets = [Planet]()
+let planetData = [
+    "Mercury": "A very hot planet, closest to the sun.",
+    "Venus": "It's very cloudy here!",
+    "Earth": "There is something very familiar about this planet.",
+    "Mars": "Known as the red planet.",
+    "Jupiter": "A gas giant, with a noticeable red spot.",
+    "Saturn": "This planet has beautiful rings around it.",
+    "Uranus": "Strangely, this planet rotates around on its side.",
+    "Neptune": "A very cold planet, furthest from the sun."
+]
 
-planets.append(mercury)
-planets.append(venus)
-planets.append(earth)
-planets.append(mars)
-planets.append(jupiter)
-planets.append(saturn)
-planets.append(uranus)
-planets.append(neptune)
+let planets = map(planetData) { name, description in Planet(name: name, description: description) }
 
 let solarSystem = PlanetarySystem(name: systemName, planets: planets)
 let adventure = SpaceAdventure(planetarySystem: solarSystem)
