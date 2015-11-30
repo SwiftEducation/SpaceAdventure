4f8fe084655edddfd8028b1070fefaea9d2599b3
Steve Hayman <steve@hayman.net>
2015-11-30 12:30:15 -0500

    Update to Xcode 7.1.1 and Swift 2. References issue #4

--- SpaceAdventure/SpaceAdventure.swift
+++ SpaceAdventure/SpaceAdventure.swift
@@ -12,26 +12,26 @@ class SpaceAdventure {
     func start() {
         displayIntroduction()
         greetAdventurer()
-        println("Let's go on an adventure!")
+        print("Let's go on an adventure!")
         determineDestination()
     }
  
     private func displayIntroduction() {
         let numberOfPlanets = 8
         let diameterOfEarth = 24859.82 // In miles, from pole to pole.
-        println("Welcome to our solar system!")
-        println("There are \(numberOfPlanets) planets to explore.")
-        println("Your are currently on Earth, which has a circumference of \(diameterOfEarth) miles.")
+        print("Welcome to our solar system!")
+        print("There are \(numberOfPlanets) planets to explore.")
+        print("Your are currently on Earth, which has a circumference of \(diameterOfEarth) miles.")
     }
     
     private func responseToPrompt(prompt: String) -> String {
-        println(prompt)
+        print(prompt)
         return getln()
     }
 
     private func greetAdventurer() {
         let name = responseToPrompt("What is your name?")
-        println("Nice to meet you, \(name). My name is Eliza, I'm an old friend of Siri.")
+        print("Nice to meet you, \(name). My name is Eliza, I'm an old friend of Siri.")
     }
     
     private func determineDestination() {
@@ -39,13 +39,13 @@ class SpaceAdventure {
         while !(decision == "Y" || decision == "N") {
             decision = responseToPrompt("Shall I randomly choose a planet for you to visit? (Y or N)")
             if decision == "Y" {
-                println("Ok! Traveling to...")
+                print("Ok! Traveling to...")
                 // TOOD: travel to random planet
             } else if decision == "N" {
-                println("Ok, name the planet you would like to visit...")
+                print("Ok, name the planet you would like to visit...")
                 // TODO: let the user select a planet to visit
             } else {
-                println("Sorry, I didn't get that.")
+                print("Sorry, I didn't get that.")
             }
         }
     }
