4ed741115fe62ab60927c5c98d20c0c96407236c
Steve Hayman <steve@hayman.net>
2015-11-30 12:50:14 -0500

    Update to Xcode 7.1.1 and Swift 2. References issue #4

--- SpaceAdventure/SpaceAdventure.swift
+++ SpaceAdventure/SpaceAdventure.swift
@@ -19,24 +19,24 @@ class SpaceAdventure {
         displayIntroduction()
         greetAdventurer()
         if (!planetarySystem.planets.isEmpty) {
-            println("Let's go on an adventure!")
+            print("Let's go on an adventure!")
             determineDestination()
         }
     }
  
     private func displayIntroduction() {
-        println("Welcome to the \(planetarySystem.name)!")
-        println("There are \(planetarySystem.planets.count) planets to explore.")
+        print("Welcome to the \(planetarySystem.name)!")
+        print("There are \(planetarySystem.planets.count) planets to explore.")
     }
     
     private func responseToPrompt(prompt: String) -> String {
-        println(prompt)
+        print(prompt)
         return getln()
     }
 
     private func greetAdventurer() {
         let name = responseToPrompt("What is your name?")
-        println("Nice to meet you, \(name). My name is Eliza, I'm an old friend of Siri.")
+        print("Nice to meet you, \(name). My name is Eliza, I'm an old friend of Siri.")
     }
     
     private func determineDestination() {
@@ -47,22 +47,22 @@ class SpaceAdventure {
                 if let planet = planetarySystem.randomPlanet {
                     visit(planet.name)
                 } else {
-                    println("Sorry, but there are no planets in this system.")
+                    print("Sorry, but there are no planets in this system.")
                 }
             } else if decision == "N" {
                 let planetName = responseToPrompt("Ok, name the planet you would like to visit.")
                 visit(planetName)
             } else {
-                println("Sorry, I didn't get that.")
+                print("Sorry, I didn't get that.")
             }
         }
     }
     
     private func visit(planetName: String) {
-        println("Traveling to \(planetName)...")
+        print("Traveling to \(planetName)...")
         for planet in planetarySystem.planets {
             if planetName == planet.name {
-                println("Arrived at \(planet.name). \(planet.description)")
+                print("Arrived at \(planet.name). \(planet.description)")
             }
         }
     }
