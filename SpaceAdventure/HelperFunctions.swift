fd1fb0fba56ae4ec9e28d817a9fd46434e024e53
Gabe McFarlane <gabrielmcfarlane65@gmail.com>
2017-02-16 15:03:15 -0800

    Update for Xcode 8 and Swift 3. References #8.
    Signed-off-by: Gabe McFarlane<gabrielmcfarlane65@gmail.com>
    Reviewed-by: Yong Bakos <ybakos@humanoriented.com>

--- SpaceAdventure/HelperFunctions.swift
+++ SpaceAdventure/HelperFunctions.swift
@@ -10,8 +10,8 @@ import Foundation
 // Wait for the user to type something in the console, and return what
 // they type as a String with the trailing newline character removed.
 func getln() -> String {
-    let stdin = NSFileHandle.fileHandleWithStandardInput()
-    var input = NSString(data: stdin.availableData, encoding: NSUTF8StringEncoding)
-    input = input!.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+    let stdin = FileHandle.standardInput
+    var input = NSString(data: stdin.availableData, encoding: String.Encoding.utf8.rawValue)
+    input = input!.trimmingCharacters(in: CharacterSet.newlines) as NSString?
     return input as! String
 }
